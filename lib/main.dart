33478286d88d95c90ac53010896cbc08a050216f
Varuna Amachi <varunamachi@gmail.com>
2022-02-20 19:11:11 +0530

    Initial project structure

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: Scaffold(
+          appBar: AppBar(title: const Text("Hello")),
+          body: const Text("hello!"),
+        ));
+  }
+}
---
+++ lib/router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// Route<dynamic> onGenerateRoute(RouteSettings settings) {
+//   return Route(settings: settings);
+// }
+
+// PageRouteBuilder<dynamic> _build(Widget screen) {
+//   return PageRouteBuilder<dynamic>(
+//     pageBuilder: (BuildContext ctx, __, ___) {
+
+//     },
+//     transitionsBuilder: (_, anim, __, child) {
+//       return FadeTransition(opacity: anim, child: child);
+//     },
+//   );
+// }
+
+// void navigateTo(BuildContext ctx, String path) {
+//   Navigator.pushNamed(ctx, path);
+// }
+
+// void popNav(BuildContext ctx, String route, {Object? args}) {
+//   Navigator.of(ctx).pushNamedAndRemoveUntil(
+//     route,
+//     (Route<dynamic> route) => false,
+//   );
+// }
